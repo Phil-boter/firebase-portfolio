d95817078b0da2ec637e20b7af249827f268ce70
Phil-boter <phildwd@gmail.com>
2021-10-25 09:53:21 +0200

    test scrolltotop button incl click event

--- src/components/scrollToTopButton/Index.test.js
+++ src/components/scrollToTopButton/Index.test.js
@@ -1,14 +1,28 @@
 import React from "react";
 
 import { shallow } from "enzyme";
+
 // import toJson from "enzyme-to-json";
 import ScrollToTopButton from "./Index";
 
 const showScroll = true;
+global.scrollTo = jest.fn();
+const windowOff = (window.pageYOffset = 401);
 
 let wrapper = shallow(<ScrollToTopButton showScroll={showScroll} />);
 describe("ScrolltoTpop component", () => {
     it("should render the ScrollToTop Button", () => {
         expect(wrapper.find(".scroll-top-button").length).toBe(1);
+    });
+    it("should not render the ScrollToTop Button when offset < 400", () => {
+        let offWrapper = shallow(<ScrollToTopButton showScroll={false} />);
+        expect(offWrapper.find(".scroll-top-button").length).toBe(0);
+    });
+    it("scrolls to position top:0 when button is clicked", () => {
+        const scrollToSpy = jest.fn();
+        global.scrollTo = scrollToSpy;
+        wrapper.find(".scroll-top-button").simulate("click");
+        expect(scrollToSpy).toHaveBeenCalled();
+        expect(window.scrollTo).toBeCalledWith({ top: 0, behavior: "smooth" });
     });
 });
